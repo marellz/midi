722e524ecf06258f94f27c0d47c5bb5cbac8b6e5
Dave Njoroge <davenjoroge10@gmail.com>
2023-05-30 18:28:32 +0300

    api: entity models/migrations - sector, entity,memeber, type

---
+++ api/app/Models/entity/Type.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\entity;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Type extends Model
+{
+    use HasFactory;
+}
